d96955148b0c326545fd5d631fe648edaf6978e9
EthanShipston <36234265+EthanShipston@users.noreply.github.com>
2018-06-11 14:39:07 -0400

    Update Background.cs

--- Background.cs
+++ Background.cs
@@ -12,33 +12,16 @@ namespace ITTD
 {
     class Background
     {
-        public void drawWalls(Canvas c)
+        public void drawMap1(Canvas c)
         {
-            Rectangle leftWall = new Rectangle();
-            leftWall.Height = c.Height;
-            leftWall.Width = 10;
-            leftWall.Fill = Brushes.Red;
-            c.Children.Add(leftWall);
-
             Rectangle floor = new Rectangle();
             floor.Width = c.Width;
             floor.Height = 10;
             floor.Fill = Brushes.Red;
             Canvas.SetBottom(floor, 0);
             c.Children.Add(floor);
-
-            Rectangle rightWall = new Rectangle();
-            rightWall.Height = c.Height;
-            rightWall.Width = 10;
-            rightWall.Fill = Brushes.Red;
-            Canvas.SetRight(rightWall, 0);
-            c.Children.Add(rightWall);
-
-            Rectangle roof = new Rectangle();
-            roof.Height = 10;
-            roof.Width = c.Width;
-            roof.Fill = Brushes.Red;
-            c.Children.Add(roof);
         }
     }
+    
+
 }
