7631eb69848fd9b0e4ece876936c080c044a43be
KeeganC <36234333+KeeganC@users.noreply.github.com>
2018-06-15 11:43:14 -0400

    Update Bullet.cs

--- Bullet.cs
+++ Bullet.cs
@@ -15,14 +15,18 @@ namespace ITTD
     {
         double bulletX;
         public Rectangle bullet;
+        Canvas canvas;
 
         public Bullet(Canvas c, bool facingLeft, double gunBarrelX, double gunBarrelY)
         {
+            canvas = c;
+
             //create bullet
             bullet = new Rectangle();
             bullet.Width = 5;
             bullet.Height = 5;
             bullet.Fill = Brushes.Green;
+
             //check which side of player to spawn the bullet on
             if (facingLeft == false)
             {
@@ -32,9 +36,18 @@ namespace ITTD
             {
                 bulletX = gunBarrelX - 5;
             }
-            c.Children.Add(bullet);
+            canvas.Children.Add(bullet);
             Canvas.SetLeft(bullet, bulletX);
             Canvas.SetBottom(bullet, gunBarrelY + 15);
+        }
+        public void update()
+        {
+            bulletX += 10;
+            Canvas.SetLeft(bullet, bulletX); 
+        }
+        public void removeBullet()
+        {
+            canvas.Children.Remove(bullet);
         }
     }
 }
