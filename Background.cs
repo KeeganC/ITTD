16b2d7d3dca40d47c56433cae5ea10d8e66faa64
EthanShipston <36234265+EthanShipston@users.noreply.github.com>
2018-06-17 15:04:23 -0400

    Update Background.cs

--- Background.cs
+++ Background.cs
@@ -13,11 +13,11 @@ namespace ITTD
     class Background
     {
         public enum Maps { Map1 }
-        public void drawPlatform(Canvas c, Point location, double width, Brush colour)
+        public void drawPlatform(Canvas c, Point location, double width, Brush colour, double height)
         {
             Rectangle rectangle = new Rectangle();
             rectangle.Width = width;
-            rectangle.Height = 20;
+            rectangle.Height = height;
             rectangle.Fill = colour;
             Canvas.SetBottom(rectangle, location.Y);
             Canvas.SetLeft(rectangle, location.X);
@@ -26,20 +26,31 @@ namespace ITTD
         public void drawMap1(Canvas c)
         {
             Point locFloor = new Point(0, 0);
-            drawPlatform(c, locFloor, c.Width, Brushes.Red);
+            drawPlatform(c, locFloor, c.Width, Brushes.DarkGreen, 20);
 
-            Point locPlatform = new Point(100, 100);
-            drawPlatform(c, locPlatform, 100, Brushes.Blue);
+            Point locPlatform = new Point();
+            locPlatform.Y = 20;
+            locPlatform.X = c.Width / 2 - 50;
+            drawPlatform(c, locPlatform, 100, Brushes.Black, 70);
+
+            locPlatform.X = 100;
+            locPlatform.Y = 100;
+            drawPlatform(c, locPlatform, 100, Brushes.Blue, 10);
+
+            
 
             locPlatform.X = 600;
-            drawPlatform(c, locPlatform, 100, Brushes.Blue);
+            drawPlatform(c, locPlatform, 100, Brushes.Blue, 10);
 
             locPlatform.X = 250;
             locPlatform.Y = 170;
-            drawPlatform(c, locPlatform, 300, Brushes.Black);
+            drawPlatform(c, locPlatform, 300, Brushes.Black, 20);
+
+            locPlatform.X = 300;
+            locPlatform.Y = 230;
+            drawPlatform(c, locPlatform, 200, Brushes.Blue, 10);
         }
     }
     
 
 }
-
